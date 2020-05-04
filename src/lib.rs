cf44167a50e436a7995b3c86a22c21efb3dbb8da
Daniel Olano <daniel@olanod.com>
2020-05-04 16:31:01 +0200

    Make account generic and plan for HD wallet

--- src/lib.rs
+++ src/lib.rs
@@ -2,38 +2,59 @@
 //! manage private keys of different kinds saved in a secure storage.
 #![no_std]
 
+#[macro_use]
 extern crate alloc;
-use alloc::string::{String, ToString};
+use alloc::borrow::ToOwned;
+use alloc::boxed::Box;
+use alloc::string::String;
 use alloc::vec::Vec;
+
+use hashbrown::HashMap;
 
 /// Wallet is the main interface to manage and interact with accounts.  
 pub struct Wallet {
-    accounts: Vec<Account>,
+    seed: String,
+    accounts: HashMap<String, Box<dyn Account>>,
 }
 
 impl Wallet {
-    /// Create new wallet
-    pub fn new() -> Self {
-        Self {
-            accounts: Vec::new(),
+    /// Import a wallet from its mnemonic seed
+    /// ```
+    /// # use libwallet::Wallet;
+    /// let wallet = Wallet::import("test test test test");
+    /// ```
+    pub fn import(seed: &str) -> Self {
+        Wallet {
+            seed: seed.to_owned(),
+            accounts: HashMap::new(),
         }
     }
 
-    /// import an account from a seed
+    /// Add an account with a given name that is derived
+    /// from the master key of the wallet.  
     /// ```
     /// # use libwallet::Wallet;
-    /// # let mut wallet = Wallet::new();
-    /// wallet.import_account("test test test test");
+    /// # let mut wallet = Wallet::import("");
+    /// wallet.add_account("moneyyy");
     /// ```
-    pub fn import_account(&mut self, seed: &str) {
-        self.accounts.push(Account {
-            seed: seed.to_string(),
-            pk: Vec::new(),
-        });
+    pub fn add_account(&mut self, name: &str) -> impl Account {
+        let account = self.derive(name);
+        self.accounts.insert(name.to_owned(), Box::new(account));
     }
+
+    fn derive(&self, _phrase: &str) -> impl Account {}
 }
 
-pub struct Account {
-    pk: Vec<u8>,
-    seed: String,
+pub trait Account {
+    fn address(&self) -> Vec<u8>;
+    fn pk(&self) -> Vec<u8>;
+}
+
+impl Account for () {
+    fn address(&self) -> Vec<u8> {
+        vec![0]
+    }
+    fn pk(&self) -> Vec<u8> {
+        vec![0]
+    }
 }
