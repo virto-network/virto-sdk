0a38f063d44826d076573accd20559078a0c021e
Daniel Olano <daniel@olanod.com>
2020-05-18 18:53:44 +0200

    Add std error support

--- src/lib.rs
+++ src/lib.rs
@@ -85,6 +85,9 @@ pub enum Error {
     InvalidPhrase,
 }
 
+#[cfg(feature = "std")]
+impl std::error::Error for Error {}
+
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
