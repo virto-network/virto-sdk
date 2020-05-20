df07de0dde2c84c6b53cfdcdedb0e345d90d7418
Daniel Olano <daniel@olanod.com>
2020-05-20 15:33:13 +0200

    Create secure Seed type
    Using zeroize to derive Drop the drop trait to clean the seed from
    memory on drop.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,19 @@
 //! With `libwallet` you can build crypto currency wallets that
 //! manage private keys of different kinds saved in a secure storage.
 use async_trait::async_trait;
-pub use bip39::{Language, Mnemonic, MnemonicType, Seed};
+pub use bip39::{Language, Mnemonic, MnemonicType};
 use sp_core::{crypto::CryptoType, sr25519, Pair};
+use std::convert::TryFrom;
+use std::fmt;
+use zeroize::Zeroize;
 
 #[cfg(feature = "chain")]
 pub mod chain;
 
 #[async_trait]
-pub trait Valut {
+pub trait Vault {
     async fn store(&self, secret: &[u8]) -> Result<()>;
-    async fn unlock(&self, password: &str) -> Result<Seed>;
+    async fn unlock(&self, password: String) -> Result<Seed>;
 }
 
 type Result<T> = std::result::Result<T, Error>;
@@ -19,7 +22,7 @@ type Result<T> = std::result::Result<T, Error>;
 #[derive(Default)]
 pub struct Wallet<'a> {
     seed: Option<Seed>,
-    vault: Option<&'a dyn Valut>,
+    vault: Option<&'a dyn Vault>,
 }
 
 impl<'a> Wallet<'a> {
@@ -27,7 +30,7 @@ impl<'a> Wallet<'a> {
         Default::default()
     }
 
-    pub fn with_vault(self, vault: &'a dyn Valut) -> Self {
+    pub fn with_vault(self, vault: &'a dyn Vault) -> Self {
         Wallet {
             vault: Some(vault),
             ..self
@@ -35,30 +38,30 @@ impl<'a> Wallet<'a> {
     }
 
     /// Generate a new wallet with a 24 word english mnemonic seed
-    pub fn generate(password: &str) -> (Self, String) {
+    pub fn generate(password: String) -> (Self, String) {
         let phrase = mnemonic(Language::English);
-        (Wallet::import(&phrase, password).unwrap(), phrase)
+        (Wallet::import(phrase.clone(), password).unwrap(), phrase)
     }
 
     /// Import a wallet from its mnemonic seed
     /// ```
     /// # use libwallet::{Language, Wallet, mnemonic};
     /// let phrase = mnemonic(Language::English);
-    /// let mut wallet = Wallet::import(&phrase, "").unwrap();
+    /// let mut wallet = Wallet::import(phrase, "foo".to_owned()).unwrap();
     /// # assert_eq!(wallet.is_locked(), false);
     /// ```
-    pub fn import(seed_phrase: &str, password: &str) -> Result<Self> {
-        let mnemonic = Mnemonic::from_phrase(seed_phrase, Language::English)
+    pub fn import(seed_phrase: String, password: String) -> Result<Self> {
+        let mnemonic = Mnemonic::from_phrase(&seed_phrase, Language::English)
             .map_err(|_| Error::InvalidPhrase)?;
-        let seed = Some(Seed::new(&mnemonic, password));
+        let seed = bip39::Seed::new(&mnemonic, &password).into();
         Ok(Wallet {
-            seed,
+            seed: Some(seed),
             ..Default::default()
         })
     }
 
     pub fn default_account(&self) -> Result<<Self as CryptoType>::Pair> {
-        let seed = self.seed.as_ref().ok_or(Error::Locked)?.as_bytes();
+        let seed = self.seed.as_ref().ok_or(Error::Locked)?.as_ref();
         let default =
             <Self as CryptoType>::Pair::from_seed_slice(&seed[..32]).expect("seed is valid");
         Ok(default)
@@ -66,31 +69,31 @@ impl<'a> Wallet<'a> {
 
     /// A locked wallet can use a vault to retrive its secret seed.
     /// ```
-    /// # use libwallet::{Wallet, Error, Seed, Mnemonic, MnemonicType, Language};
-    /// # use libwallet::Valut;
+    /// # use libwallet::{Wallet, Error, Seed, mnemonic, Language, Vault};
+    /// # use std::convert::TryInto;
     /// # struct Dummy;
-    /// # #[async_trait::async_trait] impl Valut for Dummy {
+    /// # #[async_trait::async_trait] impl Vault for Dummy {
     /// #   async fn store(&self, _: &[u8]) -> Result<(), Error> { todo!() }
-    /// #   async fn unlock(&self, pwd: &str) -> Result<Seed, Error> {
-    /// #       Ok(Seed::new(&Mnemonic::new(MnemonicType::Words12, Language::English), pwd))
+    /// #   async fn unlock(&self, pwd: String) -> Result<Seed, Error> {
+    /// #       (mnemonic(Language::English), pwd).try_into()
     /// #   }
     /// # }
     /// # #[async_std::main] async fn main() -> Result<(), Error> {
     /// # let dummy_vault = Dummy{};
     /// let mut wallet = Wallet::new().with_vault(&dummy_vault);
     /// if wallet.is_locked() {
-    ///     wallet.unlock("some password").await?;
+    ///     wallet.unlock("some password".to_owned()).await?;
     /// }
     /// # assert_eq!(wallet.is_locked(), false);
     /// # Ok(())
     /// # }
     /// ```
-    pub async fn unlock(&mut self, password: &str) -> Result<()> {
+    pub async fn unlock(&mut self, password: String) -> Result<()> {
         if !self.is_locked() {
             return Ok(());
         }
         let seed = self.vault.ok_or(Error::NoVault)?.unlock(password).await?;
-        self.seed = Some(seed);
+        self.seed = Some(seed).into();
         Ok(())
     }
 
@@ -101,7 +104,7 @@ impl<'a> Wallet<'a> {
     /// Sign a message with the default account and return the 512bit signature.
     /// ```
     /// # use libwallet::Wallet;
-    /// let (wallet, _) = Wallet::generate("");
+    /// let (wallet, _) = Wallet::generate("foo".to_owned());
     /// let signature = wallet.sign(&[0x01, 0x02, 0x03]);
     /// assert!(signature.is_ok());
     /// ```
@@ -113,6 +116,42 @@ impl<'a> Wallet<'a> {
 
 impl<'a> CryptoType for Wallet<'a> {
     type Pair = sr25519::Pair;
+}
+
+#[derive(Zeroize)]
+#[zeroize(drop)]
+pub struct Seed([u8; 64]);
+
+impl TryFrom<(String, String)> for Seed {
+    type Error = Error;
+
+    fn try_from((phrase, pwd): (String, String)) -> Result<Self> {
+        let seed = bip39::Seed::new(
+            &Mnemonic::from_phrase(&phrase, Language::English).map_err(|_| Error::InvalidPhrase)?,
+            &pwd,
+        );
+        Ok(seed.into())
+    }
+}
+
+impl From<bip39::Seed> for Seed {
+    fn from(seed: bip39::Seed) -> Self {
+        let mut s = [0; 64];
+        s.copy_from_slice(&seed.as_bytes()[..64]);
+        Self(s)
+    }
+}
+
+impl AsRef<[u8]> for Seed {
+    fn as_ref(&self) -> &[u8] {
+        self.0.as_ref()
+    }
+}
+
+impl fmt::Debug for Seed {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "<secret>")
+    }
 }
 
 #[derive(Debug, thiserror::Error)]
