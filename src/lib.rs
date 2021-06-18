83a24126ffbcc1dc5a9c1c4515084d433b073f44
Daniel Olano <daniel@olanod.com>
2021-06-18 17:16:26 +0200

    Test simple struct serialization

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,51 @@
+use scale_info::Type;
+use serde::Serialize;
+
+pub struct Value<'a> {
+    data: &'a [u8],
+    info: &'a Type,
+}
+
+impl<'a> Value<'a> {
+    pub fn new(data: &'a [u8], info: &'a Type) -> Self {
+        Value { data, info }
+    }
+}
+
+impl<'a> Serialize for Value<'a> {
+    fn serialize<S>(&self, _serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        todo!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use std::error::Error;
+
+    use super::*;
+    use parity_scale_codec::Encode;
+    use scale_info::TypeInfo;
+    use serde_json::to_value;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn serialize_simple_struct() -> Result<(), Box<dyn Error>> {
+        #[derive(Encode, Serialize, TypeInfo)]
+        struct Foo {
+            bar: u32,
+            baz: bool,
+        }
+        let foo = Foo {
+            bar: 123,
+            baz: true,
+        };
+        let data = foo.encode();
+        let info = Foo::type_info();
+        let val = Value::new(&data, &info);
+
+        assert_eq!(to_value(val)?, to_value(foo)?);
+        Ok(())
     }
 }
