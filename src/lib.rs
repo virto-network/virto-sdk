a99f0cb49e0dc57735786596f02f31c3abfa19d3
Stanly Johnson <stanlyjohnson@outlook.com>
2021-08-23 10:41:01 +0530

    Remove deserialzie and lint fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
+use scale_info::form::MetaForm;
+use scale_info::{Type, TypeDef};
+use serde::ser::SerializeSeq;
+use serde::ser::{SerializeStruct, SerializeTuple};
+use serde::Serialize;
+use serde_json::to_value;
+use std::any::Any;
 use std::convert::TryInto;
 use std::str;
-
-use scale_info::form::MetaForm;
-use scale_info::{Type, TypeDef, TypeDefPrimitive};
-use serde::de::Visitor;
-use serde::ser::SerializeSeq;
-use serde::ser::SerializeTuple;
-use serde::ser::{Error, SerializeStruct};
-use serde::{Deserialize, Serialize, Serializer};
 
 pub struct Value<'a> {
     data: &'a [u8],
@@ -27,7 +26,7 @@ impl<'a> Serialize for Value<'a> {
     {
         match self.info.type_def() {
             TypeDef::Primitive(primitive) => match primitive {
-                scale_info::TypeDefPrimitive::U8 => ser.serialize_u8(self.data[0].into()),
+                scale_info::TypeDefPrimitive::U8 => ser.serialize_u8(self.data[0]),
                 scale_info::TypeDefPrimitive::U16 => ser.serialize_u16(self.data[0].into()),
                 scale_info::TypeDefPrimitive::U32 => ser.serialize_u32(self.data[0].into()),
                 scale_info::TypeDefPrimitive::U64 => ser.serialize_u64(self.data[0].into()),
@@ -61,25 +60,45 @@ impl<'a> Serialize for Value<'a> {
                 for (_, f) in fields.iter().enumerate() {
                     let name = f.name().unwrap();
                     let t = f.ty().type_info();
-                    let size = get_size(t.type_def());
-                    state.serialize_field(name, &self.data[i])?;
-                    i = i + size;
+                    match t.type_def() {
+                        TypeDef::Primitive(_) => {
+                            let size = get_size(t.type_def());
+                            state.serialize_field(name, &self.data[i])?;
+                            i += size;
+                        },
+                        _ => {
+                            println!("{:?}", t.type_def());
+                            println!("{:?}", self.data);
+                            println!("{:?}", f.to_owned());
+                            let size = std::mem::size_of_val(&f.to_owned().ty());
+                            let u = &f.ty().type_info();
+                            let data  = Value::new(&self.data, u);
+                            println!("{:?}", size);
+                            state.serialize_field(name, &data)?;
+                            i += size;
+                        }
+                    }
                 }
                 state.end()
             }
             TypeDef::Variant(_y) => ser.serialize_bytes(self.data),
-            TypeDef::Sequence(_seq) => {
+            TypeDef::Sequence(x) => {
+                let size = get_size(x.type_param().type_info().type_def());
                 let mut seq = ser.serialize_seq(Some(self.data.len()))?;
-                println!("{:?}", self.data);
-                for e in self.data {
-                    seq.serialize_element(e)?;
+                let mut i: usize = 1;
+                while i < self.data.len() {
+                    seq.serialize_element(&self.data[i])?;
+                    i += size;
                 }
                 seq.end()
             }
-            TypeDef::Array(_) => {
+            TypeDef::Array(x) => {
+                let size = get_size(x.type_param().type_info().type_def());
                 let mut seq = ser.serialize_seq(Some(self.data.len()))?;
-                for e in self.data {
-                    seq.serialize_element(e)?;
+                let mut i: usize = 1;
+                while i < self.data.len() {
+                    seq.serialize_element(&self.data[i])?;
+                    i += size;
                 }
                 seq.end()
             }
@@ -89,7 +108,7 @@ impl<'a> Serialize for Value<'a> {
                 for (_, f) in x.fields().iter().enumerate() {
                     let size = get_size(f.type_info().type_def());
                     seq.serialize_element(&self.data[i])?;
-                    i = i + size;
+                    i += size;
                 }
                 seq.end()
             }
@@ -116,42 +135,7 @@ fn get_size(t: &TypeDef<MetaForm>) -> usize {
             scale_info::TypeDefPrimitive::Char => std::mem::size_of::<char>(),
             _ => 0,
         },
-        TypeDef::Composite(_) => todo!(),
-        TypeDef::Variant(_) => todo!(),
-        TypeDef::Sequence(_) => todo!(),
-        TypeDef::Array(_) => todo!(),
-        TypeDef::Tuple(_) => todo!(),
-        TypeDef::Compact(_) => todo!(),
-        TypeDef::Phantom(_) => todo!(),
-    }
-}
-
-impl<'de> Deserialize<'de> for Value<'de> {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        struct ValueVisitor;
-
-        impl<'de> Visitor<'de> for ValueVisitor {
-            type Value = Value<'de>;
-
-            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-            todo!()
-            }
-        }
-
-        deserializer.deserialize_u32(ValueVisitor)
-    
-    }
-
-    fn deserialize_in_place<D>(deserializer: D, place: &mut Self) -> Result<(), D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        // Default implementation just delegates to `deserialize` impl.
-        *place = Deserialize::deserialize(deserializer)?;
-        Ok(())
+        _ => 0, // ideally this match arm is not required
     }
 }
 
@@ -163,7 +147,6 @@ mod tests {
     use parity_scale_codec::Encode;
     use scale_info::TypeInfo;
     use serde_json::to_value;
-    use serde_json::from_slice;
 
     #[test]
     fn serialize_u32() -> Result<(), Box<dyn Error>> {
@@ -215,15 +198,35 @@ mod tests {
         Ok(())
     }
 
-    // #[test]
-    // fn serialize_array() -> Result<(), Box<dyn Error>> {
-    //     let foo: Vec<u16> = [2u16, 7u16].into();
-    //     let data = foo.encode();
-    //     let info = Vec::<u16>::type_info();
-    //     let val = Value::new(&data, &info);
-    //     assert_eq!(to_value(val)?, to_value(foo)?);
-    //     Ok(())
-    // }
+    #[test]
+    fn serialize_u8array() -> Result<(), Box<dyn Error>> {
+        let foo: Vec<u8> = [2u8, 7u8].into();
+        let data = foo.encode();
+        let info = Vec::<u8>::type_info();
+        let val = Value::new(&data, &info);
+        assert_eq!(to_value(val)?, to_value(foo)?);
+        Ok(())
+    }
+
+    #[test]
+    fn serialize_u16array() -> Result<(), Box<dyn Error>> {
+        let foo: Vec<u16> = [2u16, 7u16].into();
+        let data = foo.encode();
+        let info = Vec::<u16>::type_info();
+        let val = Value::new(&data, &info);
+        assert_eq!(to_value(val)?, to_value(foo)?);
+        Ok(())
+    }
+
+    #[test]
+    fn serialize_u32array() -> Result<(), Box<dyn Error>> {
+        let foo: Vec<u32> = [2u32, 7u32].into();
+        let data = foo.encode();
+        let info = Vec::<u32>::type_info();
+        let val = Value::new(&data, &info);
+        assert_eq!(to_value(val)?, to_value(foo)?);
+        Ok(())
+    }
 
     #[test]
     fn serialize_tuple() -> Result<(), Box<dyn Error>> {
@@ -236,7 +239,7 @@ mod tests {
     }
 
     #[test]
-    fn serialize_simple_struct() -> Result<(), Box<dyn Error>> {
+    fn serialize_simple_u32struct() -> Result<(), Box<dyn Error>> {
         #[derive(Encode, Serialize, TypeInfo)]
         struct Foo {
             bar: u32,
@@ -252,13 +255,50 @@ mod tests {
     }
 
     #[test]
-    fn deserialize_u32() -> Result<(), Box<dyn Error>> {
-        let foo = 2u32;
+    fn serialize_simple_u8struct() -> Result<(), Box<dyn Error>> {
+        #[derive(Encode, Serialize, TypeInfo)]
+        struct Foo {
+            bar: u8,
+            baz: u8,
+        }
+        let foo = Foo { bar: 123, baz: 45 };
         let data = foo.encode();
-        println!("{:?}", data);
-        let info = u32::type_info();
+        let info = Foo::type_info();
         let val = Value::new(&data, &info);
-        assert_eq!(from_slice::<u32>(val.data)?, from_slice::<u32>(&data)?);
+
+        assert_eq!(to_value(val)?, to_value(foo)?);
+        Ok(())
+    }
+
+    #[test]
+    fn serialize_simple_u64struct() -> Result<(), Box<dyn Error>> {
+        #[derive(Encode, Serialize, TypeInfo)]
+        struct Foo {
+            bar: u64,
+            baz: u64,
+        }
+        let foo = Foo { bar: 123, baz: 45 };
+        let data = foo.encode();
+        let info = Foo::type_info();
+        let val = Value::new(&data, &info);
+
+        assert_eq!(to_value(val)?, to_value(foo)?);
+        Ok(())
+    }
+
+    #[test]
+    fn serialize_complex_struct() -> Result<(), Box<dyn Error>> {
+        #[derive(Encode, Serialize, TypeInfo)]
+        struct Foo {
+            bar: Vec<u8>,
+            baz: u8,
+        }
+        let foo = Foo { bar: [123u8].into(), baz: 45 };
+        let data = foo.encode();
+        let info = Foo::type_info();
+        let val = Value::new(&data, &info);
+
+        assert_eq!(to_value(val)?, to_value(foo)?);
         Ok(())
     }
 }
