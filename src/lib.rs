c40942c35442167f065d5a999c1642d95d87c551
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2022-12-20 16:42:51 -0500

    change(lib): expose rpc and hasher as public mods

--- src/lib.rs
+++ src/lib.rs
@@ -109,10 +109,10 @@ pub mod http;
 #[cfg(feature = "ws")]
 pub mod ws;
 
-mod hasher;
+pub mod hasher;
 pub mod meta_ext;
 #[cfg(any(feature = "http", feature = "http-web", feature = "ws"))]
-mod rpc;
+pub mod rpc;
 
 /// Main interface for interacting with the Substrate based blockchain
 #[derive(Debug)]
