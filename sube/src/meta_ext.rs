64a5d3777319dec4b1461477cc25d52ba49fd93b
b-avb <brayan.vargas.dev@gmail.com>
2023-11-20 08:06:18 -0500

    chore: update dependencies

--- sube/src/meta_ext.rs
+++ sube/src/meta_ext.rs
@@ -4,7 +4,7 @@ use core::{borrow::Borrow, slice};
 use codec::Decode;
 #[cfg(any(feature = "v13"))]
 use frame_metadata::decode_different::DecodeDifferent;
-use frame_metadata::{PalletCallMetadata, RuntimeMetadata, RuntimeMetadataPrefixed};
+use frame_metadata::{RuntimeMetadata, RuntimeMetadataPrefixed};
 
 #[cfg(feature = "v13")]
 pub use v13::*;
