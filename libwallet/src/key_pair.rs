c246445fcbaa9b8c2afbb973c03e7d91805fd614
david barinas <david.barinas.dev@gmail.com>
2024-04-17 17:41:31 -0500

    feat(key_pair): Add method to convert signature to bytes

--- libwallet/src/key_pair.rs
+++ libwallet/src/key_pair.rs
@@ -1,5 +1,4 @@
-use core::{convert::TryFrom, fmt::Debug};
-
+use core::{convert::TryInto, fmt::Debug};
 pub use derive::Derive;
 
 use self::any::AnySignature;
@@ -20,7 +19,11 @@ pub trait Pair: Signer + Derive {
 pub trait Public: AsRef<[u8]> + Debug {}
 impl<const N: usize> Public for Bytes<N> {}
 
-pub trait Signature: AsRef<[u8]> + Debug + PartialEq {}
+pub trait Signature: AsRef<[u8]> + Debug + PartialEq {
+    fn as_bytes<const N: usize>(&self) -> Bytes<N> {
+        self.as_ref().try_into().expect("error")
+    }
+}
 impl<const N: usize> Signature for Bytes<N> {}
 
 /// Something that can sign messages
