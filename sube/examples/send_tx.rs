1ebac3bfe3f6e27c0722bdc0b44beaa058c9f578
david barinas <david.barinas.dev@gmail.com>
2024-04-29 00:05:59 -0500

    wip

--- sube/examples/send_tx.rs
+++ sube/examples/send_tx.rs
@@ -1,11 +1,11 @@
 use futures_util::TryFutureExt;
-use libwallet::{self, vault, Signature};
+use libwallet::{self, vault, Account, Signature};
 use rand_core::OsRng;
 use serde_json::json;
 use std::env;
 use sube::sube;
 
-type Wallet = libwallet::Wallet<vault::Simple>;
+type Wallet = libwallet::Wallet<vault::Simple<String>>;
 
 use anyhow::{anyhow, Result};
 
@@ -22,15 +22,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut wallet = Wallet::new(vault);
     wallet
-        .unlock(None)
+        .unlock(None, None)
         .await
         .map_err(|_| anyhow!("error wallet"))?;
 
     let account = wallet.default_account();
-    let public = account.public();
+    let public = account.unwrap().public();
 
     let response = sube!("wss://rococo-rpc.polkadot.io/balances/transfer" => {
-        signer: |message: &[u8]| Ok(wallet.sign(message).as_bytes()),
+        signer: async |message: &[u8]| Ok(wallet.sign(message).await.expect("hello").as_bytes()),
         sender: public.as_ref(),
         body:  json!({
             "dest": {
@@ -43,7 +43,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .map_err(|err| anyhow!(format!("SubeError {:?}", err)))?;
 
     println!("Secret phrase: \"{phrase}\"");
-    println!("Default Account: 0x{account}");
+    println!("Default Account: 0x{}", account.unwrap());
 
     Ok(())
 }
