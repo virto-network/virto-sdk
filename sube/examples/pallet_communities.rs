5a7bfc380f71e2e33d21666920adb9b93066c830
David barinas <david.barinas.dev@gmail.com>
2024-05-18 13:29:07 -0500

    fix: add scales formating to query keys (#55)

---
+++ sube/examples/pallet_communities.rs
@@ -0,0 +1,18 @@
+use async_trait::async_trait;
+
+use env_logger;
+use serde_json;
+use sube::{sube, ExtrinsicBody, Response, Result, SubeBuilder};
+
+#[async_std::main]
+async fn main() -> Result<()> {
+    env_logger::init();
+
+    let response = sube!("ws://127.0.0.1:8000/communityTracks/tracks/1").await?;
+
+     if let Response::Value(value) = response {
+        let data = serde_json::to_value(&value).expect("it must be a serialized object");
+        println!("Account info: {}", serde_json::to_string_pretty(&data).expect("it must return an str"));
+    }
+    Ok(())
+}
