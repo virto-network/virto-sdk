4f6c649c66ea3a7455ffbe5d7666df965c8bdb16
s0c5 <david.barinas.dev@gmail.com>
2024-05-23 17:03:25 -0500

    feat: Add generic deserialization for RPC requests

--- sube/src/ws.rs
+++ sube/src/ws.rs
@@ -32,6 +32,7 @@ use jsonrpc::{
     serde_json,
 };
 use log::info;
+use serde::Deserialize;
 
 use crate::{
     rpc::{self, Rpc, RpcResult},
@@ -50,7 +51,7 @@ unsafe impl Sync for Backend {}
 
 #[async_trait]
 impl Rpc for Backend {
-    async fn rpc(&self, method: &str, params: &[&str]) -> RpcResult {
+    async fn rpc<T: for<'a> Deserialize<'a>>(&self, method: &str, params: &[&str]) -> RpcResult<T> {
         let id = self.next_id().await;
         info!("RPC `{}` (ID={})", method, id);
 
@@ -68,7 +69,7 @@ impl Rpc for Backend {
         })
         .expect("Request is serializable");
 
-        log::debug!("RPC Request {} ...", &msg[..50]);
+        log::debug!("RPC Request {} ...", &msg);
 
         self.tx
             .lock()
@@ -81,12 +82,9 @@ impl Rpc for Backend {
         let res = recv
             .await
             .map_err(|_| standard_error(StandardError::InternalError, None))?
-            .result::<String>()?;
+            .result()?;
 
-        log::debug!("RPC Response: {}...", &res[..res.len().min(20)]);
-
-        let res = hex::decode(&res[2..])
-            .map_err(|_| standard_error(StandardError::InternalError, None))?;
+        // log::debug!("RPC Response: {}...", &res[..20]);
 
         Ok(res)
     }
@@ -153,14 +151,14 @@ impl Backend {
                                         ().into(),
                                     )
                                 });
-
+                            // log::info!("res {:?}", res);
                             if res.id.is_u64() {
                                 let id = res.id.as_u64().unwrap() as Id;
                                 log::trace!("Answering request {}", id);
                                 let mut messages = messages.lock().await;
                                 if let Some(channel) = messages.remove(&id) {
-                                    channel.send(res).expect("receiver waiting");
                                     log::debug!("Answered request id: {}", id);
+                                    channel.send(res).expect("receiver waiting");
                                 }
                             }
                         }
