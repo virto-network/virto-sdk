095a9533a6400307bede6b09f7d528a531d5c1b6
b-avb <brayan.vargas.dev@gmail.com>
2024-07-25 13:38:30 -0500

    wip fix timer

--- sube/examples/query_identity.rs
+++ sube/examples/query_identity.rs
@@ -7,15 +7,17 @@ use sube::{sube, Response};
 async fn main() -> sube::Result<()> {
     env_logger::init();
 
-
-    let result = sube!("ws://127.0.0.1:12281/system/account/0x12840f0626ac847d41089c4e05cf0719c5698af1e3bb87b66542de70b2de4b2b").await?;
+    let result = sube!("ws://localhost:11004/identity/superOf/0x6d6f646c6b762f636d7479738501000000000000000000000000000000000000").await?;
 
     if let Response::Value(value) = result {
         let data = serde_json::to_value(&value).expect("to be serializable");
-        println!("Account info: {}", serde_json::to_string_pretty(&data).expect("it must return an str"));
+        println!(
+            "Account info: {}",
+            serde_json::to_string_pretty(&data).expect("it must return an str")
+        );
     }
 
-    let query = format!("ws://127.0.0.1:11004/identity/identityOf/0xbe6ed76ac48d5c7f1c5d2cab8a1d1e7a451dcc24b624b088ef554fd47ba21139");
+    let query = format!("ws://localhost:11004/identity/identityOf/0xbe6ed76ac48d5c7f1c5d2cab8a1d1e7a451dcc24b624b088ef554fd47ba21139");
 
     let r = sube!(&query).await?;
 
