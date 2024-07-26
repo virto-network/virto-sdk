a9e8443936ecb3349100db04f8adbcde77a886cd
s0c5 <david.barinas.dev@gmail.com>
2024-07-26 13:24:03 +0200

    chore: uncomment pallet communities

--- sube/examples/pallet_communities.rs
+++ sube/examples/pallet_communities.rs
@@ -6,12 +6,12 @@ use sube::{sube, ExtrinsicBody, Response, Result, SubeBuilder};
 async fn main() -> Result<()> {
     env_logger::init();
 
-    // let response = sube!("ws://127.0.0.1:12281/communityMemberships/collection").await?;
+    let response = sube!("ws://127.0.0.1:12281/communityMemberships/collection").await?;
 
-    // if let Response::ValueSet(value) = response {
-    //     let data = serde_json::to_value(&value).expect("to be serializable");
-    //     println!("Collection {}", serde_json::to_string_pretty(&data).expect("it must return an str"));
-    // }
+    if let Response::ValueSet(value) = response {
+        let data = serde_json::to_value(&value).expect("to be serializable");
+        println!("Collection {}", serde_json::to_string_pretty(&data).expect("it must return an str"));
+    }
 
     let result = sube!("https://kreivo.io/system/account/0x12840f0626ac847d41089c4e05cf0719c5698af1e3bb87b66542de70b2de4b2b").await?;
 
