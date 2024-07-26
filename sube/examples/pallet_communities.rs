35882fe1b7759d731181cf5172a70ef0e29841c1
s0c5 <david.barinas.dev@gmail.com>
2024-07-26 13:16:33 +0200

    enchancement: update the core to reqwest

--- sube/examples/pallet_communities.rs
+++ sube/examples/pallet_communities.rs
@@ -1,5 +1,3 @@
-use async_trait::async_trait;
-
 use env_logger;
 use serde_json;
 use sube::{sube, ExtrinsicBody, Response, Result, SubeBuilder};
@@ -8,14 +6,14 @@ use sube::{sube, ExtrinsicBody, Response, Result, SubeBuilder};
 async fn main() -> Result<()> {
     env_logger::init();
 
-    let response = sube!("ws://127.0.0.1:12281/communityMemberships/collection").await?;
+    // let response = sube!("ws://127.0.0.1:12281/communityMemberships/collection").await?;
 
-    if let Response::ValueSet(value) = result {
-        let data = serde_json::to_value(&value).expect("to be serializable");
-        println!("Collection {}", serde_json::to_string_pretty(&data).expect("it must return an str"));
-    }
+    // if let Response::ValueSet(value) = response {
+    //     let data = serde_json::to_value(&value).expect("to be serializable");
+    //     println!("Collection {}", serde_json::to_string_pretty(&data).expect("it must return an str"));
+    // }
 
-    let result = sube!("ws://127.0.0.1:12281/system/account/0x12840f0626ac847d41089c4e05cf0719c5698af1e3bb87b66542de70b2de4b2b").await?;
+    let result = sube!("https://kreivo.io/system/account/0x12840f0626ac847d41089c4e05cf0719c5698af1e3bb87b66542de70b2de4b2b").await?;
 
     if let Response::Value(value) = result {
         let data = serde_json::to_value(&value).expect("to be serializable");
--- sube/examples/query_balance.rs
+++ sube/examples/query_balance.rs
@@ -1,4 +1,3 @@
-use async_trait::async_trait;
 use core::future::{Future, IntoFuture};
 use sube::{sube, Response, Result};
 
