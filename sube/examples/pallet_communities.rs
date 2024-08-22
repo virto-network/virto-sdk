6475b8622a98e661b0da0254efa68f350f69eb98
s0c5 <david.barinas.dev@gmail.com>
2024-08-22 09:59:37 +0200

    fix: examples

--- sube/examples/pallet_communities.rs
+++ sube/examples/pallet_communities.rs
@@ -18,25 +18,25 @@ async fn main() -> Result<()> {
         );
     }
 
-    // let response = sube!("ws://127.0.0.1:12281/communityMemberships/collection").await?;
+    let response = sube!("ws://127.0.0.1:12281/communityMemberships/collection").await?;
 
-    // if let Response::ValueSet(value) = response {
-    //     let data = serde_json::to_value(&value).expect("to be serializable");
-    //     println!(
-    //         "Collection {}",
-    //         serde_json::to_string_pretty(&data).expect("it must return an str")
-    //     );
-    // }
+    if let Response::ValueSet(value) = response {
+        let data = serde_json::to_value(&value).expect("to be serializable");
+        println!(
+            "Collection {}",
+            serde_json::to_string_pretty(&data).expect("it must return an str")
+        );
+    }
 
-    // let result = sube!("https://kreivo.io/system/account/0x12840f0626ac847d41089c4e05cf0719c5698af1e3bb87b66542de70b2de4b2b").await?;
+    let result = sube!("https://kreivo.io/system/account/0x12840f0626ac847d41089c4e05cf0719c5698af1e3bb87b66542de70b2de4b2b").await?;
 
-    // if let Response::Value(value) = result {
-    //     let data = serde_json::to_value(&value).expect("to be serializable");
-    //     println!(
-    //         "Account info: {}",
-    //         serde_json::to_string_pretty(&data).expect("it must return an str")
-    //     );
-    // }
+    if let Response::Value(value) = result {
+        let data = serde_json::to_value(&value).expect("to be serializable");
+        println!(
+            "Account info: {}",
+            serde_json::to_string_pretty(&data).expect("it must return an str")
+        );
+    }
 
     Ok(())
 }
