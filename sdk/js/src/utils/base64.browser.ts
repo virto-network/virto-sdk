83ccf4a5630d225d7eb28ea5f0750e7081329f52
b-avb <brayan.vargas.dev@gmail.com>
2025-08-31 18:52:46 -0500

    change: update authenticators

--- sdk/js/src/utils/base64.browser.ts
+++ sdk/js/src/utils/base64.browser.ts
@@ -1,5 +1,5 @@
 import 'es-arraybuffer-base64/Uint8Array.fromBase64/auto';
 
 export function fromBase64Url(str: string): ArrayBuffer {
-    return Uint8Array.fromBase64(str, { alphabet: 'base64url' })
+    return Uint8Array.fromBase64(str, { alphabet: 'base64url' }) as unknown as ArrayBuffer;
 }
